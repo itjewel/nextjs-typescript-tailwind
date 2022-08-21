5c93d2526b1731180885f90b9db539d8c652c954
md.jewel mia <md.jewelmia52@gmail.com>
2022-08-21 19:14:28 +0600

    scroll added

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,9 +41,7 @@ const Home = ({
           <Row title="Trending Now" movies={trendingNow} />
           <Row title="Top Rated" movies={topRated} />
           <Row title="Action Thrillers" movies={actionMovies} />
-          {/* My List */}
-          {/* {list.length > 0 && <Row title="My List" movies={list} />} */}
-
+   
           <Row title="Comedies" movies={comedyMovies} />
           <Row title="Scary Movies" movies={horrorMovies} />
           <Row title="Romance Movies" movies={romanceMovies} />
